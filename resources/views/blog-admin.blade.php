72708c59ae2f928af00cdf037b8477bb6e83accf
srmartigan <srmartigan@gmail.com>
2019-08-03 02:35:16 +0200

    Añadiendo panel administrador y empezar a configurarlo

--- resources/views/blog-admin.blade.php
+++ resources/views/blog-admin.blade.php
@@ -1,2 +1,12 @@
-<h1>Esta sera la pantalla de administracion del blog</h1>
-<p>Esto avanza sigue asi!!!!</p>
+@extends('layaout/admin')
+
+@section('titulo','Admin -')
+
+@section('head')
+    {--Aqui podemos añadir librerias--}
+@endsection
+
+@section('contenido')
+    <h1>Esta sera la pantalla de administracion del blog</h1>
+    <p>Esto avanza sigue asi!!!!</p>
+@endsection
